5341730ff4f902074c7de1309707485239f2db0d
Takashi IIGUNI <iiguni.tks@gmail.com>
2020-06-04 07:13:49 +0000

    implement StateSubcommand

--- runt/src/subcommand/mod.rs
+++ runt/src/subcommand/mod.rs
@@ -1,14 +1,16 @@
 pub mod create;
 pub mod spec;
+pub mod state;
 
 use anyhow::Result;
 use clap::ArgMatches;
 
-use crate::subcommand::{create::CreateCommand, spec::SpecCommand};
+use crate::subcommand::{create::CreateCommand, spec::SpecCommand, state::StateCommand};
 
 pub enum SubCommand {
     Create(CreateCommand),
     Spec(SpecCommand),
+    State(StateCommand),
 }
 
 pub trait SubCommandImpl: Sized {
